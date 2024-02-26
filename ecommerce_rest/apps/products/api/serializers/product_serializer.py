812b677e982661ccf1a532d683800458fcdc1e57
Juan Pablo Listte <juanpilistte@gmail.com>
2024-02-26 18:30:23 -0300

    update crud

--- ecommerce_rest/apps/products/api/serializers/product_serializer.py
+++ ecommerce_rest/apps/products/api/serializers/product_serializer.py
@@ -13,7 +13,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'id': instance.id,
             'name': instance.name,
             'description': instance.description,
-            'image': instance.image if instance.image != '' else '',
-            'measure_unit': instance.measure_unit.description,
-            'category_product': instance.category_product.description
+            'image': instance.image if instance.image else None,
+            'measure_unit': instance.measure_unit.description if instance.measure_unit is not None else '',
+            'category_product': instance.category_product.description if instance.category_product is not None else ''
         }
--- ecommerce_rest/apps/products/api/views/products_view.py
+++ ecommerce_rest/apps/products/api/views/products_view.py
@@ -4,11 +4,10 @@ from apps.products.api.serializers.product_serializer import ProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
-class ProductListAPIView(GeneralListAPIView):
-    serializer_class = ProductSerializer
     
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
+    queryset = ProductSerializer.Meta.model.objects.filter(state=True)
     
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -17,17 +16,28 @@ class ProductCreateAPIView(generics.CreateAPIView):
             return Response({'message':'Product created successfully!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state=True)
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state=True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(id=pk, state=True).first()
 
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    serializer_class = ProductSerializer
+    def patch(self, request, pk=None):
+        if self.get_queryset(pk):
+            product_serializer = self.serializer_class(self.get_queryset(pk))
+            return Response(product_serializer.data, status=status.HTTP_200_OK)
+        return Response({"error": "The selected product doesn't exist."}, status=status.HTTP_400_BAD_REQUEST)
     
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state=True)
+    def put(self, request, pk=None):
+        if self.get_queryset(pk):
+            product_serializer = self.serializer_class(self.get_queryset(pk), data=request.data)
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status=status.HTTP_200_OK)
+            return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk=None):
         product = self.get_queryset().filter(id=pk).first()
