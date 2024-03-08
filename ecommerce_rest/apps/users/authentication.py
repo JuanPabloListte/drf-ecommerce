7f9766aee25016beeb847f6dafc2af7d306691db
Juan Pablo Listte <juanpilistte@gmail.com>
2024-03-08 19:45:28 -0300

    updating tokens

--- ecommerce_rest/apps/users/authentication.py
+++ ecommerce_rest/apps/users/authentication.py
@@ -5,9 +5,7 @@ from django.utils import timezone
 from django.conf import settings
 
 class ExpiringTokenAuthentication(TokenAuthentication):
-    
-    expired = False
-    
+       
     def expires_in(self, token):
         time_elapsed = timezone.now() - token.created
         left_time = timedelta(seconds=settings.TOKEN_EXPIRED_AFTER_SECONDS) - time_elapsed
@@ -19,31 +17,21 @@ class ExpiringTokenAuthentication(TokenAuthentication):
     def token_expire_handler(self, token):
         is_expired = self.is_token_expired(token)
         if is_expired:
-            self.expired = True
             user = token.user
             token.delete()
             token = self.get_model().objects.create(user=user)
             
-        return is_expired, token
+        return token
     
     def authenticate_credentials(self, key):
-        message, token, user = None, None, None
+        user = None
         try:
             token = self.get_model().objects.select_related('user').get(key=key)
+            token = self.token_expire_handler(token)
             user = token.user
-        
         except self.get_model().DoesNotExist:
-            message = 'Invalid token'
-            self.expired = True
+            pass
         
-        if token is not None:
-            if not token.user.is_active:
-                message = 'Inactive or deleted user'
-        
-            is_expired = self.token_expire_handler(token)
-            if is_expired:
-                message = 'The token has expired'
-        
-        return (user, token, message, self.expired)
+        return user
         
         
--- ecommerce_rest/apps/users/authentication_mixins.py
+++ ecommerce_rest/apps/users/authentication_mixins.py
@@ -8,7 +8,6 @@ from rest_framework import status
 class Authentication(object):
     
     user = None
-    user_token_expired = False
 
     def get_user(self, request):
         token = get_authorization_header(request).split()
@@ -19,26 +18,17 @@ class Authentication(object):
                 return None
             
             token_expired = ExpiringTokenAuthentication()
-            user, token, message, self.user_token_expired = token_expired.authenticate_credentials(token)
-            if user != None and token != None:
+            user = token_expired.authenticate_credentials(token)
+            if user != None:
                 self.user = user
                 return user
-            return message
         return None
     
     def dispatch(self, request, *args, **kwargs):
         user = self.get_user(request)
         if user is not None:
-            if isinstance(user, str):
-                response = Response({'error': user, 'Expired': self.user_token_expired}, 
-                                    status=status.HTTP_400_BAD_REQUEST)
-                response.accepted_renderer = JSONRenderer()
-                response.accepted_media_type = 'application/json'
-                response.renderer_context = {}
-                return response
-            if not self.user_token_expired:
-                return super().dispatch(request, *args, **kwargs)
-        response =  Response({'error': 'The credentials have not been sent', 'Expired': self.user_token_expired}, 
+            return super().dispatch(request, *args, **kwargs)
+        response =  Response({'error': 'The credentials have not been sent'}, 
                              status=status.HTTP_400_BAD_REQUEST)
         response.accepted_renderer = JSONRenderer()
         response.accepted_media_type = 'application/json'
